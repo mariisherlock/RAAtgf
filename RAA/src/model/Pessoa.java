f3ee884b7d85c4eb818807e3906378a4357bd1ee
AlmerindoOten <almerindo.neto17@gmail.com>
2026-07-04 16:45:55 -0300

    Mundança feita em Pessoa e Usuario, passando id para Pessoa

--- RAA/src/model/Pessoa.java
+++ RAA/src/model/Pessoa.java
@@ -1,6 +1,8 @@
 package model;
 
 public abstract class Pessoa {
+
+    private int id;
     private String nome;
 
     public Pessoa() {
@@ -12,10 +14,18 @@ public abstract class Pessoa {
 
     public abstract void criarConta();
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
