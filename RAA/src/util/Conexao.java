b3893f7ea02757dff2ad3c799af2414fc1f54b94
AlmerindoOten <almerindo.neto17@gmail.com>
2026-07-05 15:25:25 -0300

    Atualização - Versão final do projeto

--- RAA/src/util/Conexao.java
+++ RAA/src/util/Conexao.java
@@ -6,21 +6,12 @@ import java.sql.SQLException;
 
 public class Conexao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/RAA";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/RAA";
     private static final String USUARIO = "root";
-    private static final String SENHA = "oitodigitoS8";
-
-    static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Driver MySQL carregado.");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver NÃO encontrado!");
-            e.printStackTrace();
-        }
-    }
+    private static final String SENHA = "Oten0704";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
     }
+
 }
